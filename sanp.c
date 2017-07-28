4fe031dc8374c37608013538bc6db8f8aba05986
19ece <30343283+19ece@users.noreply.github.com>
2017-07-29 01:05:03 +0530

    Create sanp.c

---
+++ sanp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[i]={'a','e','i','o','u'},consonant;
+printf("Enter the character");
+scanf("%c",&consonant);
+if(a[i]==consonant)
+{
+printf("%c is vowel");
+}
+else
+{
+printf("%c is consonent");
+}
+getch();
+}
