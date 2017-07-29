d130d4117865f74e3b2167c191af4cef3b9f56a6
19ece <30343283+19ece@users.noreply.github.com>
2017-07-29 16:59:33 +0530

    Create sathap.c

---
+++ sathap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num1,num2,num3;
+printf("Enter the numbers a,b,c");
+scanf("%d %d %d",a,b,c);
+if(a<b)
+{
+a=b;
+if(b<c)
+{
+printf("The largest number is  %d");
+}
+}
+}
