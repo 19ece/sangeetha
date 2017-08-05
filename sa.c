4f0cea59072a9df7004a12235e56b0ff398d17ce
19ece <30343283+19ece@users.noreply.github.com>
2017-08-05 16:57:15 +0530

    Create sa.c

---
+++ sa.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char alphabet;
+printf("Enther the charater");
+scanf("%c",&alphabet);
+if(alphabet==%c)
+{
+printf("Given charater is alphabet");
+}
+else
+{
+printf("Given charater is not an alphabet");
+}
+}
