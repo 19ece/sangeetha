81a9e53c404b7f043c329e214953fd68f6edebc6
19ece <30343283+19ece@users.noreply.github.com>
2017-09-05 15:35:24 +0530

    Create sumloop.c

---
+++ sumloop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,sum=0;
+printf("Enter the n value:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("sum of n number is;",sum);
+return o;
+}
