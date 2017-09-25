da33d4bdd1e38bc7cad03c6c8faf189aa890b61e
19ece <30343283+19ece@users.noreply.github.com>
2017-09-25 18:17:50 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n, m,val;
+printf("Enter the base of the power");
+scanf("%d",&n);
+printf("The power of base is");
+scanf("%d",&m);
+val=n^m;
+printf("The power of given number is",val);
+return o;
+}
